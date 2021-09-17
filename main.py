285869a7d819e767b14f0fe4e76d974f738f7c30
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:49:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Run code block 1 and examine the error message.
+# Fix the syntax error then run the code again to check your work.
+
+# Code block 1:
+launch_ready = False
+fuel_level = 17000
+
+if fuel_level >= 20000
+  print('Fuel level cleared.')
+  launch_ready = True
+else:
+  print('WARNING: Insufficient fuel!')
+  launch_ready = False
+
+# Code block 2 hides two syntax errors.
+# Un-comment lines 18 - 34. Run the code and find the mistakes. Only ONE error will be flagged at a time. 
+
+# crew_status = True
+# computer_status = 'green'
+
+# if crew_status and computer_status = 'green':
+#   print('Crew & computer cleared.')
+#   launch_ready = True
+# else:
+#   print('WARNING: Crew or computer not ready!')
+#   launch_ready = False
+
+# if (launch_ready):
+#   print("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...")
+#   print("Fed parrot...")
+#   print("Ignition...")
+#   print("Liftoff!')
+# else:
+#   print("Launch scrubbed.")
